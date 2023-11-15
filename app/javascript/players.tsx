fd59ae690fad2a9ba4b15d9fc0586a69f86ba85b
Daniel Hoey <danielhoey1@gmail.com>
2023-11-15 14:17:46 +0800

    Use Formik

--- app/javascript/players.tsx
+++ app/javascript/players.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {renderReactApp} from "./util";
+import {ErrorMessage, Field, Form, Formik, FormikBag, FormikHelpers, FormikState, FormikValues} from "formik";
 
 export const PlayersList = () => {
 
@@ -10,7 +11,16 @@ export const PlayersList = () => {
     //@ts-ignore
     const playerCells = (p) => fields.map((f,i) => <td key={i}>{p[f]}</td>);
 
+    let addPlayer = (values:any, formikBag:FormikHelpers<any>) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+        formikBag.setSubmitting(false);
+      }, 1000)
+    };
+
     return (
+      <div>
+
       <table>
         <thead>
           <tr>
@@ -30,6 +40,21 @@ export const PlayersList = () => {
           {players.map((p) => <tr key={p.id}>{playerCells(p)}</tr> )}
         </tbody>
       </table>
+
+        <Formik initialValues={{first_name: '', last_name:'', number:null}} onSubmit={addPlayer}>
+          {( a:FormikState<any> ) => {
+            return (
+              <Form>
+                <Field type="text" name="first_name"/> <ErrorMessage name="first_name" component="div"/>
+                <Field type="text" name="last_name"/> <ErrorMessage name="last_name" component="div"/>
+                <Field type="number" name="number"/> <ErrorMessage name="number" component="div"/>
+                <button type="submit" disabled={a.isSubmitting}>Submit</button>
+              </Form>
+            );
+          }}
+        </Formik>
+      </div>
+
     );
   };
 
